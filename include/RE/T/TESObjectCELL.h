441291199ccc11ecd8f96501d7dd86c6113aab20
max-su-2019 <54713650+max-su-2019@users.noreply.github.com>
2022-01-29 16:41:12 +0800

    Add "TESObjectCell::PlaceParticleEffect"

--- include/RE/T/TESObjectCELL.h
+++ include/RE/T/TESObjectCELL.h
@@ -19,6 +19,7 @@
 
 namespace RE
 {
+	class BSTempEffectParticle;
 	class bhkWorld;
 	class NavMesh;
 	class NiNode;
@@ -185,6 +186,8 @@ namespace RE
 			REL::Relocation<func_t> func{ REL::ID(18995) };
 			return func(this);
 		}
+
+		BSTempEffectParticle* PlaceParticleEffect(float a_lifetime, const char* a_modelName, const NiMatrix3& a_normal, const NiPoint3& a_pos, float a_scale, std::uint32_t a_flags, NiAVObject* a_target);
 
 		void           ForEachReference(std::function<bool(TESObjectREFR&)> a_callback) const;
 		void           ForEachReferenceInRange(const NiPoint3& a_origin, float a_radius, std::function<bool(TESObjectREFR&)> a_callback) const;
